70893528b089eea7e5a6c6d75f48be8946bfa18d
Michael Purdy <michael@mrmichael.co.uk>
2014-12-27 23:52:49 +0000

    remove query string auth

--- mrmichael/settings.py
+++ mrmichael/settings.py
@@ -106,6 +106,7 @@ STATICFILES_DIRS = (
 
 if ON_HEROKU:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    AWS_QUERYSTRING_AUTH = False
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
